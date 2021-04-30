8806d5fb618d9abfdaca505640ff3fb35c9007f3
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-04-30 21:39:08 +0300

    router

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,11 +1,17 @@
-import { useParams } from "react-router";
-
+import { Route, useParams } from "react-router";
+import Comments from "../components/comments/Comments";
 const QuoteDetail = () => {
   const params = useParams();
   return (
     <section>
       <h1>Quote Detail</h1>
       <p>{params.quoteId}</p>
+      {/* <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route> */}
+      <Route path="/quotes/:quoteId/comments">
+        <Comments />
+      </Route>
     </section>
   );
 };
